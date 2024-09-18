fb5ccf8b1af5b0d4389e2d9a77a96772260a094e
Kyryllova Sofiia <77674021+DvalStuff@users.noreply.github.com>
2024-09-18 21:13:59 +0300

    Update CartesianToSpherical.cpp

--- code/CartesianToSpherical.cpp
+++ code/CartesianToSpherical.cpp
@@ -6,14 +6,14 @@ const double pi = 3.14159265358979323846;
 
 void cartesianToSpherical(double x, double y, double z, double& r, double& theta, double& phi) {
     r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-    theta = acos(z / r);                 
-    phi = atan2(y, x);                    
+    theta = atan2(y, x);                 
+    phi = acos(z / r);                
 }
 
 void sphericalToCartesian(double r, double theta, double phi, double& x, double& y, double& z) {
-    x = r * sin(phi) * cos(theta);         
-    y = r * sin(phi) * sin(theta);         
-    z = r * cos(phi);                 
+    x = r * sin(phi) * cos(theta);
+    y = r * sin(phi) * sin(theta);
+    z = r * cos(phi);
 }
 
 int main() {
@@ -30,13 +30,13 @@ int main() {
 
     cout << endl;
 
-    double spherical_r[] = { 9.0, 5.0, 5.0 };
-    double spherical_theta[] = { pi / 4, pi / 3, pi / 4 };
-    double spherical_phi[] = { pi / 5, pi / 3, -((2*pi) / 3) };
+    double spherical_r[] = { 8.77, 5.39, 5.39 };
+    double spherical_theta[] = { pi / 4, pi / 3, -((2 * pi) / 3) };
+    double spherical_phi[] = { pi / 5, pi / 3,  pi / 4 };
 
     for (int i = 0; i < num_points; i++) {
         double x, y, z;
-        sphericalToCartesian(spherical_r[i], spherical_phi[i], spherical_theta[i], x, y, z);
+        sphericalToCartesian(spherical_r[i], spherical_theta[i], spherical_phi[i], x, y, z);
         cout << "Spherical coordinates (r = " << spherical_r[i] << ", theta = " << spherical_theta[i] << ", phi = " << spherical_phi[i] << ") in the Cartesian system: (" << x << ", " << y << ", " << z << ")" << endl;
     }
     return 0;
